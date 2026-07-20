89326febbbaf9f85f1d183d943e6396e99ab8103
nuhmanparamban <nuhmanp@gmail.com>
2026-07-20 13:27:04 -0400

    fix: import signOut in vendor layout

--- src/app/vendor/layout.tsx
+++ src/app/vendor/layout.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useSession } from 'next-auth/react';
+import { useSession, signOut } from 'next-auth/react';
 import { useRouter, usePathname } from 'next/navigation';
 import Link from 'next/link';
 import { useEffect } from 'react';
